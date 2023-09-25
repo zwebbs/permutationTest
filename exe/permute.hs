6cce92a450af5371830a820fa287a61c5aecba9a
zwebbs <zwebbs2021@gmail.com>
2023-09-25 18:16:37 -0500

    first commit

---
+++ exe/permute.hs
@@ -0,0 +1,45 @@
+-- File Name: permute.hs
+-- Created By: ZW
+-- Created On: 2023-09-25
+-- Purpose: define functions that generate list permutations 
+--   for our permutation test.
+
+import System.Random
+
+
+{--
+ define a function which takes a random generator G,
+ a list A, and the length of the list L, and returns a
+ 3-tuple of: (1) a randomly selected list element,
+ (2) the remainder of the list minus the selected element,
+ and (3) the output random generator that randomR gives us after completion
+--}
+randomSelect :: RandomGen g => g -> [a] -> Int -> (a, [a], g)
+randomSelect _ [] _ = error "Cannot Select on Empty List"
+randomSelect g [x] _ = (x, [], g)
+randomSelect g (x:xs) l = ((x:xs) !! i, (take i (x:xs)) ++ (drop (i+1) (x:xs)), g1) where 
+    (i, g1) = randomR (0, l-1) g
+
+
+{-- 
+ define a function that takes a random generator g
+ and a list A and return a shuffled list of A 
+ where the elements are rearranged.
+--}
+shuffle :: RandomGen g => g -> [a] -> Int -> [a]
+shuffle _ [] _ = []
+shuffle _ [x] _ = [x]
+shuffle g (x:xs) l =  [v1] ++ v2 where
+    (v1,r1,g1) = randomSelect g (x:xs) l
+    v2 = shuffle g1 r1 (l-1)
+
+
+
+{--
+ define a function that takes a random generator g, a list A, the length
+ of A, l, and the number of shuffles N and returns a list of lists where
+ each element is a shuffled ordering of A. the number of these shuffled
+ orderings is determined by N.
+--}
+getShuffleSet :: (RandomGen g) => g -> [a] -> Int -> Int -> [[a]]
+--getShuffleSet :: iterate 
